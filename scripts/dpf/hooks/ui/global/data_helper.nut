f239b2db46024ef457212e5f0db8c0aaf24ae1ca
taroeld <tarob@live.de>
2022-07-19 09:28:57 +0200

    refactor: simplify perk UI

--- scripts/dpf/hooks/ui/global/data_helper.nut
+++ scripts/dpf/hooks/ui/global/data_helper.nut
@@ -9,26 +9,4 @@
 		}
 		return result;
 	}
-
-	o.convertPerksToUIData = function()
-	{
-		return ::Const.Perks.DefaultPerkTree.getTree();
-	}
-
-	o.convertPerkToUIData = function( _perkID, _background )
-	{
-		local perk = _background.getPerkTree().getPerk(_perkID);
-
-		if (perk != null)
-		{
-			return {
-				id = perk.ID,
-				name = perk.Name,
-				description = perk.Tooltip,
-				imagePath = perk.Icon
-			};
-		}
-
-		return null;
-	}
 });
--- ui/mods/dpf_mod_screens.js
+++ ui/mods/dpf_mod_screens.js
@@ -1,110 +1,12 @@
 // character_screen_identifier.js
-
-CharacterScreenIdentifier.Perk.Tree = 'perkTree';
-
-// character_screen_datasource.js
-
-CharacterScreenDatasource.prototype.queryPerkInformation = function(_perkId, _background, _callback)
-{
-	this.notifyBackendQueryPerkInformation(_perkId, _background, _callback);
-};
-
-CharacterScreenDatasource.prototype.notifyBackendQueryPerkInformation = function (_perkId, _background, _callback)
-{
-	SQ.call(this.mSQHandle, 'onQueryPerkInformation', [_perkId, _background], _callback);
-};
+var DynamicPerks = {};
 
 // character_screen_perks_module.js
 
-CharacterScreenPerksModule.prototype.resetPerkTree = function(_perkTree)
-{
-	if (_perkTree == null)
-		return;
-
-	this.mPerkTree = _perkTree;
-
-	for (var row = 0; row < this.mPerkRows.length; ++row)
-	{
-		this.mPerkRows[row].removeClass('is-unlocked').addClass('is-locked');
-	}
-
-	for (var row = 0; row < _perkTree.length; ++row)
-	{
-		for (var i = 0; i < _perkTree[row].length; ++i)
-		{
-			var perk = _perkTree[row][i];
-			console.error(Object.keys(perk));
-			perk.Unlocked = false;
-
-			perk.Image.attr('src', Path.GFX + perk.IconDisabled);
-
-			var selectionLayer = perk.Container.find('.selection-image-layer:first');
-			selectionLayer.addClass('display-none').removeClass('display-block');
-		}
-	}
-};
-
-CharacterScreenPerksModule.prototype.setupPerkTree = function (_perkTree)
-{
-	if (this.mPerkTree !== null) {
-		this.removePerksEventHandlers()
-	}
-	this.mLeftColumn.empty();
-	this.mPerkTree = _perkTree;
-	this.createPerkTreeDIV(this.mPerkTree, this.mLeftColumn);
-
-	this.setupPerksEventHandlers(this.mPerkTree);
-};
-
+DynamicPerks.loadPerkTreesWithBrotherData = CharacterScreenPerksModule.prototype.loadPerkTreesWithBrotherData;
 CharacterScreenPerksModule.prototype.loadPerkTreesWithBrotherData = function (_brother)
 {
-	this.setupPerkTree(_brother[CharacterScreenIdentifier.Perk.Tree]);
-
-	if (CharacterScreenIdentifier.Perk.Key in _brother)
-	{
-		this.initPerkTree(this.mPerkTree, _brother[CharacterScreenIdentifier.Perk.Key]);
-	}
-
-	if (CharacterScreenIdentifier.Entity.Id in _brother)
-	{
-		this.setupPerkTreeTooltips(this.mPerkTree, _brother[CharacterScreenIdentifier.Entity.Id]);
-	}
-};
-
-CharacterScreenPerksModule.prototype.isPerkUnlockable = function (_perk)
-{
-	var _brother = this.mDataSource.getSelectedBrother();
-	var character = _brother[CharacterScreenIdentifier.Entity.Character.Key];
-	var level = character[CharacterScreenIdentifier.Entity.Character.Level];
-	var perkPoints = this.mDataSource.getBrotherPerkPoints(_brother);
-	var perkPointsSpent = this.mDataSource.getBrotherPerkPointsSpent(_brother);
-
-	if(level >= 13 && _perk.ID === 'perk.student') {
-		return false;
-	}
-	return perkPoints > 0 && perkPointsSpent >= _perk.Unlocks;
-};
-
-CharacterScreenPerksModule.prototype.setupPerksEventHandlers = function(_perkTree)
-{
-	//this.removePerksEventHandlers();
-
-	for (var row = 0; row < _perkTree.length; ++row)
-	{
-		for (var i = 0; i < _perkTree[row].length; ++i)
-		{
-			var perk = _perkTree[row][i];
-			this.attachEventHandler(perk);
-		}
-	}
-};
-
-CharacterScreenPerksModule.prototype.onPerkTreeLoaded = function (_dataSource, _perkTree)
-{
-	// if (_perkTree !== null)
-	// {
-
-	// 	this.mPerkTree = _perkTree;
-	//	 this.setupPerkTree();
-	// }
+	this.resetPerkTree(this.mPerkTree);
+	this.onPerkTreeLoaded(null, _brother.perkTree);
+	DynamicPerks.loadPerkTreesWithBrotherData.call(this, _brother);
 };
