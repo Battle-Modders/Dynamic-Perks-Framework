6ffe5dc35c55fea669845d06be09362b44f7c8ba
LordMidas <55047920+LordMidas@users.noreply.github.com>
2022-07-19 03:37:50 -0400

    refactor: rename variable

--- ui/mods/dpf_mod_screens.js
+++ ui/mods/dpf_mod_screens.js
@@ -1,12 +1,8 @@
-// character_screen_identifier.js
-var DynamicPerks = {};
-
-// character_screen_perks_module.js
-
-DynamicPerks.loadPerkTreesWithBrotherData = CharacterScreenPerksModule.prototype.loadPerkTreesWithBrotherData;
+var DynamicPerksFramework = {};
+DynamicPerksFramework.loadPerkTreesWithBrotherData = CharacterScreenPerksModule.prototype.loadPerkTreesWithBrotherData;
 CharacterScreenPerksModule.prototype.loadPerkTreesWithBrotherData = function (_brother)
 {
 	this.resetPerkTree(this.mPerkTree);
 	this.onPerkTreeLoaded(null, _brother.perkTree);
-	DynamicPerks.loadPerkTreesWithBrotherData.call(this, _brother);
+	DynamicPerksFramework.loadPerkTreesWithBrotherData.call(this, _brother);
 };
