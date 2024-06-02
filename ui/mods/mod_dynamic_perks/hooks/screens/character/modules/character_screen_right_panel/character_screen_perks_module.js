933c525b9412320e1ebfddfe87455439520a71b6
taroeld <tarob@live.de>
2024-06-02 17:29:11 +0200

    feat: add highlighting of perk groups in the character screen

--- ui/mods/mod_dynamic_perks/hooks/screens/character/modules/character_screen_right_panel/character_screen_perks_module.js
+++ ui/mods/mod_dynamic_perks/hooks/screens/character/modules/character_screen_right_panel/character_screen_perks_module.js
@@ -1,15 +1,32 @@
-DynamicPerks.Hooks.loadPerkTreesWithBrotherData = CharacterScreenPerksModule.prototype.loadPerkTreesWithBrotherData;
+DynamicPerks.Hooks.CharacterScreenPerksModule_createDIV = CharacterScreenPerksModule.prototype.createDIV;
+CharacterScreenPerksModule.prototype.createDIV = function (_parentDiv)
+{
+	DynamicPerks.Hooks.CharacterScreenPerksModule_createDIV.call(this, _parentDiv);
+	this.mPerkGroups = {};
+	this.mPerkGroupColors = ["rgba(255,234,125,0.5)", "blue", "red", "green", "purple", "orange", "teal", "cyan"];
+}
+
+DynamicPerks.Hooks.CharacterScreenPerksModule_destroyDIV = CharacterScreenPerksModule.prototype.destroyDIV;
+CharacterScreenPerksModule.prototype.destroyDIV = function ()
+{
+	DynamicPerks.Hooks.CharacterScreenPerksModule_destroyDIV.call(this);
+	this.mPerkGroups = {};
+}
+
+DynamicPerks.Hooks.CharacterScreenPerksModule_loadPerkTreesWithBrotherData = CharacterScreenPerksModule.prototype.loadPerkTreesWithBrotherData;
 CharacterScreenPerksModule.prototype.loadPerkTreesWithBrotherData = function (_brother)
 {
 	this.resetPerkTree(this.mPerkTree);
 	this.onPerkTreeLoaded(null, _brother.perkTree);
-	DynamicPerks.Hooks.loadPerkTreesWithBrotherData.call(this, _brother);
+	DynamicPerks.Hooks.CharacterScreenPerksModule_loadPerkTreesWithBrotherData.call(this, _brother);
 };
 
 CharacterScreenPerksModule.prototype.initPerkTree = function (_perkTree, _perksUnlocked)
 {
+	var self = this;
 	var perkTier = this.mDataSource.getBrotherPerkTier(this.mDataSource.getSelectedBrother());
 	var lockedPerks = this.mDataSource.getLockedPerks(this.mDataSource.getSelectedBrother());
+	this.mPerkGroups = {};
 
 	for (var row = 0; row < this.mPerkRows.length; ++row)
 	{
@@ -21,6 +38,17 @@ CharacterScreenPerksModule.prototype.initPerkTree = function (_perkTree, _perksU
 		for (var i = 0; i < _perkTree[row].length; ++i)
 		{
 			var perk = _perkTree[row][i];
+			var perkGroupOverlay = $('<div class="dynamicperks-image-overlay"/>');
+			perk.Container.append(perkGroupOverlay);
+			perk.PerkGroupOverlay = perkGroupOverlay;
+			$.each(perk.PerkGroupIDs, function(_idx, _id){
+				if (_id in self.mPerkGroups) {
+					self.mPerkGroups[_id].push(perk);
+				}
+				else {
+					self.mPerkGroups[_id] = [perk];
+				}
+			})
 			var imageLayer = perk.Container.find('.perk-image-layer:first')
 			if (row >= perkTier)
 			{
@@ -51,6 +79,62 @@ CharacterScreenPerksModule.prototype.initPerkTree = function (_perkTree, _perksU
 					break;
 				}
 			}
+		}
+	}
+};
+
+DynamicPerks.Hooks.CharacterScreenPerksModule_attachEventHandler = CharacterScreenPerksModule.prototype.attachEventHandler;
+CharacterScreenPerksModule.prototype.attachEventHandler = function(_perk)
+{
+	DynamicPerks.Hooks.CharacterScreenPerksModule_attachEventHandler.call(this, _perk);
+	var self = this;
+	var mouseOn = false;
+
+	_perk.Container.on('mouseenter.dynamicperks focus.dynamicperks' + CharacterScreenIdentifier.KeyEvent.PerksModuleNamespace, null, this, function (_event)
+	{
+		mouseOn = true;
+	});
+
+	_perk.Container.on('mouseleave.dynamicperks blur.dynamicperks' + CharacterScreenIdentifier.KeyEvent.PerksModuleNamespace, null, this, function (_event)
+	{
+		mouseOn = false;
+		if (!MSU.getSettingValue(DynamicPerks.ID, "PerkTree_HighlightPerkGroups")) return;
+
+		$.each(_perk.PerkGroupIDs, function(_idx, _id){
+			$.each(self.mPerkGroups[_id], function(_, _perk){
+				_perk.PerkGroupOverlay.css("border", "none");
+			})
+		})
+	});
+
+	$(document).on('keydown.dynamicperks' + CharacterScreenIdentifier.KeyEvent.PerksModuleNamespace, null, this, function (_event)
+	{
+		if (!mouseOn)
+			return;
+		if (!MSU.getSettingValue(DynamicPerks.ID, "PerkTree_HighlightPerkGroups"))
+			return;
+		if (!MSU.Keybinds.isKeybindPressed(DynamicPerks.ID, "PerkTree_HighlightPerkGroups_keybind", _event))
+			return;
+		Screens.Tooltip.getModule('TooltipModule').hideTooltip();
+		$.each(_perk.PerkGroupIDs, function(_idx, _id){
+			$.each(self.mPerkGroups[_id], function(_, _innerPerk){
+				if (_perk ==_innerPerk) return;
+				_innerPerk.PerkGroupOverlay.css("border", "2px solid " + self.mPerkGroupColors[_idx]);
+			})
+		})
+	});
+}
+
+DynamicPerks.Hooks.CharacterScreenPerksModule_removePerksEventHandler = CharacterScreenPerksModule.prototype.removePerksEventHandler;
+CharacterScreenPerksModule.prototype.removePerksEventHandler = function (_perkTree)
+{
+	DynamicPerks.Hooks.CharacterScreenPerksModule_removePerksEventHandler.call(this, _perkTree);
+	for (var row = 0; row < _perkTree.length; ++row)
+	{
+		for (var i = 0; i < _perkTree[row].length; ++i)
+		{
+			var perk = _perkTree[row][i];
+			perk.Container.off("dynamicperks");
 		}
 	}
 };
